d12db41def4a8396fa6ad6bea5e12149785ac951
Hana <hana_fakhouri@hotmail.com>
2024-10-31 14:42:59 +0200

    menu cities and menu drivers added

--- main.py
+++ main.py
@@ -1,3 +1,52 @@
+def driversMenu():
+    while True:
+        print("1. View all Drivers")
+        print("2. Add a driver")
+        print("3. Check similar drivers")
+        print("4. Go back to the main menu")
+
+        user_input = input()
+
+        if not user_input.isdigit():
+            print("Please enter a number: 1, 2, 3, 4: ")
+        else:
+            if user_input == '1':
+                print('view')
+            elif user_input == '2':
+                print('add')
+            elif user_input == '3':
+                print('check')
+            elif user_input == '4':
+                break
+            else:
+                print("Please choose 1, 2, 3, 4: ")
+
+def citiesMenu():
+    while True:
+        print("1. View Cities")
+        print("2. Search City")
+        print("3. Print neighboring cities ")
+        print("4. Print Drivers delivering to city ")
+        print("5. Go back to the main menu")
+
+        user_input = input()
+
+        if not user_input.isdigit():
+            print("Please enter a number: 1, 2, 3, 4, 5: ")
+        else:
+            if user_input == '1':
+                print('view')
+            elif user_input == '2':
+                print('search')
+            elif user_input == '3':
+                print('print')
+            elif user_input == '4':
+                print('print')
+            elif user_input == '5':
+                break
+            else:
+                print("Please choose 1, 2, 3, 4, 5: ")
+
 def main():
     print("Hello! Please enter: ")
 
@@ -12,11 +61,11 @@ def main():
             print("Please enter a number: 1, 2 or 3: ")
         else:
             if user_input == '1':
-                #driversMenu()
-                continue
+                print("Enter: ")
+                driversMenu()
             elif user_input == '2':
-                #citiesMenu()
-                continue
+                print("Enter: ")
+                citiesMenu()
             elif user_input == '3':
                 print("Thank you. Exiting the system. Exit")
                 break
